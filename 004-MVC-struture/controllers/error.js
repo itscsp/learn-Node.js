34cec42a68e49e1140d2787ce23ffa77a3ccff50
itscsp <chethanspoojary1@gmail.com>
2025-04-05 18:57:19 +0530

    Altering Project to A Ecommerce App

--- 004-MVC-struture/controllers/error.js
+++
@@ -1,7 +0,0 @@
-exports.get404 = (req, res, next) => {
-    res.status(404).render('404', {
-      pageTitle: 'Page Not Found',
-      path: '/404' // or leave empty if you don’t want to highlight any nav
-    });
-  };
-  
