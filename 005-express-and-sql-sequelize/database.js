b00f5268ede68a84db171587e75e32423018847b
itscsp <chethanspoojary1@gmail.com>
2025-04-22 09:49:57 +0530

    Building order funcationlaity using MongoDB without mongooes

--- 005-express-and-sql-sequelize/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('my_new_db', 'root', 'root', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
